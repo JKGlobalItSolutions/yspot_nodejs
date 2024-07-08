e81169d2707d3e03ed0a04db5efc22fa3bde221a
jagadish <jkglobalitsolution@gmail.com>
2024-07-08 18:15:44 +0530

    changes new

--- single-hotel-page/script.js
+++
@@ -1,5 +0,0 @@
-function scrollCarousel(direction) {
-    const container = document.getElementById('carouselContainer');
-    const scrollAmount = 270; // Adjust scroll amount as needed
-    container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
-  }
